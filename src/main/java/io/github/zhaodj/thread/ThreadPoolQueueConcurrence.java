3f4ff5c31a22d7500157fa25232a6ab420cc8386
zhaodj <zhaodj1985@gmail.com>
2019-01-30 22:58:56 +0800

    线程池队列差异

--- src/main/java/io/github/zhaodj/thread/ThreadPoolQueueConcurrence.java
+++ src/main/java/io/github/zhaodj/thread/ThreadPoolQueueConcurrence.java
@@ -14,42 +14,32 @@ public class ThreadPoolQueueConcurrence {
     public static void main(String[] args) {
         BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(1);
         //BlockingQueue<Runnable> queue = new SynchronousQueue<>();
-        ExecutorService consumer = new ThreadPoolExecutor(1, 100, 1, TimeUnit.MINUTES, queue);
+        ExecutorService consumer = new ThreadPoolExecutor(1, 10, 1, TimeUnit.MINUTES, queue);
 
-        Thread producer1 = new Thread(() -> {
-            while(true) {
-                try {
-                    consumer.submit(new Task("task1"));
-                } catch (Exception e) {
-                    Log.print("size: " + ((ThreadPoolExecutor) consumer).getPoolSize());
-                    e.printStackTrace();
-                }
-                try {
-                    Thread.sleep(20);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        Thread producer2 = new Thread(() -> {
-            while (true) {
-                try {
-                    consumer.submit(new Task("task2"));
-                } catch (Exception e) {
-                    Log.print("size: " + ((ThreadPoolExecutor) consumer).getPoolSize());
-                    e.printStackTrace();
-                }
-                try {
-                    Thread.sleep(20);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        Thread producer1 = createProducer(consumer, "producer1");
+        Thread producer2 = createProducer(consumer, "producer2");
 
         producer1.start();
         producer2.start();
+    }
+
+    private static Thread createProducer(ExecutorService consumer, String producerName){
+       return new Thread(() -> {
+            for(int i = 0; i < 5; i++) {
+                String name = producerName + ".task" + i;
+                try {
+                    consumer.submit(new Task(name));
+                } catch (Exception e) {
+                    Log.print("exception: " + name);
+                    e.printStackTrace();
+                }
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public static class Task implements Runnable {
@@ -63,7 +53,8 @@ public class ThreadPoolQueueConcurrence {
         @Override
         public void run() {
             try {
-                Thread.sleep(10);
+                Log.print("run: " + name);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
